b6d5c4d9f10fdd8e7f63c1b996852e859554fa90
Ehsan Mohammadi <ehsanx64@gmail.com>
2022-02-08 05:45:35 +0330

    Code clean-up and reformat

--- gorm/sqlite.go
+++ gorm/sqlite.go
@@ -27,25 +27,34 @@ func main() {
 	add_records(db)
 }
 
+// Migrate the schema
 func migrate(db *gorm.DB) {
-	// Migrate the schema
 	db.AutoMigrate(&Product{}, &ProductData{})
 }
 
+// CRUD operations on model
 func add_records(db *gorm.DB) {
 	// Create
 	db.Create(&Product{Title: "Laptop", Price: 100})
 
 	// Read
 	var product Product
-	db.First(&product, 1)                     // find product with integer primary key
-	db.First(&product, "title = ?", "Laptop") // find product with code D42
+	db.First(&product, 1)                     // find with primary key
+	db.First(&product, "title = ?", "Laptop") // find with title
 
 	// Update - update product's price to 200
 	db.Model(&product).Update("Price", 200)
+
 	// Update - update multiple fields
-	db.Model(&product).Updates(Product{Price: 200, Title: "Tablet"}) // non-zero fields
-	db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Title": "Ultrabook"})
+	db.Model(&product).Updates(Product{
+		Price: 200,
+		Title: "Tablet",
+	}) // non-zero fields
+
+	db.Model(&product).Updates(map[string]interface{}{
+		"Price": 200,
+		"Title": "Ultrabook",
+	})
 
 	// Delete - delete product
 	db.Delete(&product, 1)
